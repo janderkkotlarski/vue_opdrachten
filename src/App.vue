6ddf93f75bb1f024f58dfb7d61303afdacf27c1c
unknown <j.d.kotlarski@gmx.com>
2025-11-25 15:39:47 +0100

    Hopefully this will work when eventually pushed

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const clicks = ref(0);
+
+function incrementClicks() {
+  ++clicks.value;
+}
+
+const person = reactive({ firstName: "", country: "" });
+
+function changeCountry(newCountry) {
+  person.country = newCountry;
+}
+</script>
+
+<template>
+  <button @click="incrementClicks">
+    Hoe vaak heb je hier geklikt: {{ clicks }}
+  </button>
+
+  <br /><br />
+
+  Naam: {{ person.firstName }}<br />
+  Land: {{ person.country }}<br />
+
+  <button @click="changeCountry('Nederland')">Nederland</button>
+  <button @click="changeCountry('België')">België</button><br />
+
+  <input v-model.trim="person.firstName" type="text" />
+</template>
+
+<style scoped></style>
