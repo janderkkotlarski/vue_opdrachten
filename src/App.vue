7d796d71201a1003c42e7b2101f2df3446145564
unknown <j.d.kotlarski@gmx.com>
2025-11-26 14:49:28 +0100

    vue-s function system is a tad peculiar when using it in the hybrid html template section

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,20 @@ const person = reactive({ firstName: "", country: "" });
 function changeCountry(newCountry) {
   person.country = newCountry;
 }
+
+const tasks = ref([
+  { name: "Boodschappen doen", completed: false },
+  { name: "Afwassen", completed: true },
+  { name: "Hond uitlaten", completed: false },
+]);
+
+function completedness(done) {
+  return done ? "voltooid" : "niet voltooid";
+}
+
+function toggleCompletion(task) {
+  task.completed = !task.completed;
+}
 </script>
 
 <template>
@@ -28,6 +42,13 @@ function changeCountry(newCountry) {
   <button @click="changeCountry('België')">België</button><br />
 
   <input v-model.trim="person.firstName" type="text" />
+
+  <ul>
+    <li v-for="task in tasks" :key="task.id">
+      {{ task.name }} : {{ completedness(task.completed) }} :
+      <button @click="toggleCompletion(task)">wissel</button>
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
