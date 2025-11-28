01279098d0e7746836cac949edd7d9a1a6a02eb8
unknown <j.d.kotlarski@gmx.com>
2025-11-28 15:58:37 +0100

    filtering ahoy?

--- computed_properties/src/App.vue
+++ computed_properties/src/App.vue
@@ -1,25 +1,55 @@
 <script setup>
-import {ref} from 'vue';
+import {ref, computed} from 'vue';
 const people = ref([
-    {name: 'Jodocus', age: 10},
-    {name: 'Plubucus', age: 346092},
+    {id: 1, name: 'Jodocus', age: 10},
+    {id: 2, name: 'Plubucus', age: 346092},
 ]);
 
-const newName = ref();
-const newAge = ref();
+const newName = ref('');
+const newAge = ref('');
+
+function addPerson() {
+    // Only add if both fields contain something
+    if (newName.value != '' && newAge.value != '') {
+        // .value because of ref
+        people.value.push({id: people.value.length + 1, name: newName.value, age: newAge.value});
+    }
+
+    // Always flush the submitted results
+    newName.value = '';
+    newAge.value = '';
+}
+
+const children = computed(() => {
+    return [...people.value].filter();
+});
 </script>
 
 <template>
-    <div>hoi</div>
+    <form>
+        <!-- type="text" pro forma -->
+        <input v-model="newName" type="text" />
+        {{ newName }}
 
-    <input v-model="newName" />
-    {{ newName }}
+        <br />
+        <br />
 
-    <br />
-    <br />
+        <!-- .number for safe conversion, type="number" to allow only numerical input -->
+        <input v-model.number="newAge" type="number" />
+        {{ newAge }}
 
-    <input v-model="newAge" v-model.number="newAge" />
-    {{ newAge }}
+        <br />
+        <br />
+
+        <!-- .prevent in order to not reload the page -->
+        <button @click.prevent="addPerson">Durf je te klikken?</button>
+    </form>
+
+    <ul>
+        <li v-for="(person, index) in people" :key="person.id">
+            {{ person.id }} : {{ person.name }} : {{ person.age }}
+        </li>
+    </ul>
 </template>
 
 <style scoped></style>
