3151165e35f934baebf358f0b6d86b678e022787
unknown <j.d.kotlarski@gmx.com>
2025-12-02 15:55:35 +0100

    Did some extra and fancier work in order to understand the necessary inner workings of some key vue3 concepts

--- computed_properties/src/App.vue
+++ computed_properties/src/App.vue
@@ -1,16 +1,49 @@
 <script setup>
 import {ref, computed} from 'vue';
-const people = ref([
-    {id: 1, name: 'Jodocus', age: 10},
-    {id: 2, name: 'Plubucus', age: 346092},
-]);
+let ident = 0;
+
+// Make empty people array
+const people = ref([]);
+
+// Automatic testing with random generation
+// Mostly in order to understand how ref works, how arrays work
+// How to use automation and array pushing, the works
+function generatePerson() {
+    const ident = people.value.length + 1;
+
+    const person = {
+        id: ident,
+        name: 'Adama_' + ident.toString(),
+        age: Math.floor(100 * Math.random() * Math.random()) + 1,
+    };
+
+    return person;
+}
+
+// Value is important in the setup
+function pushPerson() {
+    people.value.push(generatePerson());
+}
+
+// Variable amount of people
+const amount = 5 + Math.floor(10 * Math.random());
+
+// Pushing into the people array
+function pushPersons() {
+    for (let i = 0; i < amount; ++i) {
+        pushPerson();
+    }
+}
+
+pushPersons();
 
 const newName = ref('');
 const newAge = ref('');
 
 function addPerson() {
     // Only add if both fields contain something
-    if (newName.value != '' && newAge.value != '') {
+
+    if (newName.value != '' && newAge.value > 0) {
         // .value because of ref
         people.value.push({id: people.value.length + 1, name: newName.value, age: newAge.value});
     }
@@ -20,14 +53,34 @@ function addPerson() {
     newAge.value = '';
 }
 
+const ageLimit = 18;
+
 const children = computed(() => {
-    return [...people.value].filter();
+    return people.value.filter(person => person.age < ageLimit);
+});
+
+const adults = computed(() => {
+    return people.value.filter(person => person.age >= ageLimit);
+});
+
+const peopleCount = computed(() => {
+    return people.value.length;
+});
+
+const childrenCount = computed(() => {
+    return children.value.length;
+});
+
+const adultsCount = computed(() => {
+    return adults.value.length;
 });
 </script>
 
 <template>
     <form>
         <!-- type="text" pro forma -->
+        <b>Naam</b>
+        <br />
         <input v-model="newName" type="text" />
         {{ newName }}
 
@@ -35,6 +88,8 @@ const children = computed(() => {
         <br />
 
         <!-- .number for safe conversion, type="number" to allow only numerical input -->
+        <b>Leeftijd</b>
+        <br />
         <input v-model.number="newAge" type="number" />
         {{ newAge }}
 
@@ -42,13 +97,22 @@ const children = computed(() => {
         <br />
 
         <!-- .prevent in order to not reload the page -->
-        <button @click.prevent="addPerson">Durf je te klikken?</button>
+        <button @click.prevent="addPerson">Toevoegen</button>
     </form>
 
     <ul>
-        <li v-for="(person, index) in people" :key="person.id">
-            {{ person.id }} : {{ person.name }} : {{ person.age }}
-        </li>
+        <b>{{ peopleCount }} Mensen [ Naam : Leeftijd ]</b>
+        <li v-for="person in people" :key="person.id">[ {{ person.name }} : {{ person.age }} ]</li>
+    </ul>
+
+    <ul>
+        <b>{{ childrenCount }} Kinderen [ Naam : Leeftijd ]</b>
+        <li v-for="person in children" :key="person.id">[ {{ person.name }} : {{ person.age }} ]</li>
+    </ul>
+
+    <ul>
+        <b>{{ adultsCount }} Volwassenen [ Naam : Leeftijd ]</b>
+        <li v-for="person in adults" :key="person.id">[ {{ person.name }} : {{ person.age }} ]</li>
     </ul>
 </template>
 
