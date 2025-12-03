0de92cd5879c071d0aca6437b07dd2f25c60d806
unknown <j.d.kotlarski@gmx.com>
2025-12-03 14:25:40 +0100

    successfully split off the v-for directive taks and dejeeted the vue application in the overarching folder

---
+++ v_for_directive/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue';
+
+const tasks = ref([
+    {name: 'Boodschappen doen', completed: false},
+    {name: 'Afwassen', completed: true},
+    {name: 'Hond uitlaten', completed: false},
+]);
+
+function completedness(done) {
+    return done ? 'voltooid' : 'niet voltooid';
+}
+
+function toggleCompletion(task) {
+    task.completed = !task.completed;
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="task in tasks" :key="task.id">
+            {{ task.name }} : {{ completedness(task.completed) }} :
+            <button @click="toggleCompletion(task)">wissel</button>
+        </li>
+    </ul>
+</template>
+
+<style scoped></style>
